976dcc846f2258deef497d8b77d082c7fedeb98c
woodFordR <adam@kaewell.com>
2024-10-13 00:22:20 -0400

    cached logging with lru_cache

--- ping_a_bub/config.py
+++ ping_a_bub/config.py
@@ -1,7 +1,7 @@
 # ping_a_bub/config.py
 
-
 import logging
+from functools import lru_cache
 
 from pydantic_settings import BaseSettings
 
@@ -10,9 +10,10 @@ log = logging.getLogger("uvicorn")
 
 class Settings(BaseSettings):
     environment: str = "dev"
-    testing: bool = bool(0)
+    testing: bool = 0
 
 
+@lru_cache()
 def get_settings() -> BaseSettings:
     log.info("Loading config settings from the environment ...")
     return Settings()
