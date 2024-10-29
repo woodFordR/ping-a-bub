f2deac15d5f092636da6d21ccbd809ffe7b2701d
woodFordR <adam@kaewell.com>
2024-10-28 23:38:28 -0400

    created pytest fixture for session, envs from env.test still not prevailing during pytest

--- ping-a-bub/tests/conftest.py
+++ ping-a-bub/tests/conftest.py
@@ -1,27 +1,15 @@
-# tests/conftest.py
-
-# import pytest
-# from contextlib import asynccontextmanager
-# from typing import AsyncGenerator
-# from asgi_lifespan import LifespanManager
-# from httpx import ASGITransport, AsyncClient
-# from app.main import app
+import os
+import pytest
+from sqlmodel import Session, SQLModel, create_engine
 
 
-# ClientManagerType = AsyncGenerator[AsyncClient, None]
+@pytest.fixture(name="session")
+def session_fixture():
+    engine = create_engine(
+        os.environ["DATABASE_URL"]
+    )
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
 
-
-# @asynccontextmanager
-# async def client_manager(app, base_url="http://test", **kw) -> ClientManagerType:
-#    app.state.testing = True
-#    async with LifespanManager(app):
-#        transport = ASGITransport(app=app)
-#        async with AsyncClient(transport=transport, base_url=base_url, **kw) as c:
-#            yield c
-
-
-# @pytest.fixture(scope="module")
-# async def client() -> ClientManagerType:
-#     async with client_manager(app) as c:
-#         yield c
 
--- ping-a-bub/tests/test_ping.py
+++ ping-a-bub/tests/test_ping.py
@@ -1,28 +1,22 @@
-import os
 from fastapi.testclient import TestClient
-from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel import Session
 from app.main import app, get_session
 
 
-def test_health_ping():
-    engine = create_engine(
-        os.getenv("DATABASE_URL"), connect_args={"check_same_thread": False}
-    )
+def test_health_ping(session: Session):
+    def get_session_override():
+        return session
 
-    with Session(engine) as session:
-        def get_session_override():
-            return session
+    app.dependency_overrides[get_session] = get_session_override
+    client = TestClient(app)
 
-        app.dependency_overrides[get_session] = get_session_override
-        client = TestClient(app)
+    response = client.get("/health/ping")
 
-        response = client.get("/health/ping")
+    app.dependency_overrides.clear()
+    data = response.json()
 
-        app.dependency_overrides.clear()
-        data = response.json()
-
-        assert response.status_code == 200
-        # assert data["environment"] == "testing"
-        assert data["ping_health"] == "bubs open, pong!"
+    assert response.status_code == 200
+    assert data["environment"] == "testing"
+    assert data["ping_health"] == "bubs open, pong!"
 
 
