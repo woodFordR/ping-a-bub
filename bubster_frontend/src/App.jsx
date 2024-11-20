3046380cdb18f65acd3ce5b07b4ffd4790653aec
woodFordR <adam@kaewell.com>
2024-11-19 22:44:34 -0500

    adding better header to app component title page

--- bubster_frontend/src/App.jsx
+++ bubster_frontend/src/App.jsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+const welcome = {
+  greeting: ">>welcome<<",
+  title: "bubster<<>>dashboard"
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{welcome.greeting}, {welcome.title}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </>
   )
 }
