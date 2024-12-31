8d27f7f872c26d24bc2f2b8c5da8644ae4f7f67f
woodFordR <adam@kaewell.com>
2024-12-31 16:10:40 -0500

    edited linting config to .mjs , added typescript typescript-eslint & config files for ts, edits to index to support typescript file

---
+++ bubster_frontend/eslint.config.mjs
@@ -0,0 +1,11 @@
+// typescript typecheck
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+);
+
